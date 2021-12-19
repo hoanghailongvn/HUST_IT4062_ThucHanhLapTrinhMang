40cbb31f86b8366b200ffd829497d1840e8c449c
hoanghailongvn <hoanghailong2642k@gmail.com>
2021-12-19 14:57:53 +0700

    update rq_register message

--- include/Message.h
+++ include/Message.h
@@ -4,7 +4,21 @@
 #include <iostream>
 #include <vector>
 
-enum MessageType {RQ_REGISTER = 0, RP_REGISTER};
+/*
+    Message mình truyền đi ở dạng char[]
+    Mình phải chuyển message dạng char[] sang dạng struct cho dễ xử lý hơn
+    Cần viết các hàm convert giữa 2 dạng này
+
+    Viết thêm struct message
+    Viết hàm convert giữa 2 kiểu tương ứng với messag mới
+    
+    Chuyển từ struct sang message thì dùng một hàm struct_to_message thôi
+    Nhưng chuyển từ message sang struct thì phải viets nhiều hàm do còn phụ thuộc vào kiểu trả về là struct nào
+
+*/
+
+// Nghĩ thêm các loại message rồi cho vô enum
+enum MessageType {RQ_REGISTER = 0, RP_REGISTER, RQ_LOGIN, RP_LOGIN};
 
 struct rq_register {
     MessageType type = RQ_REGISTER;
@@ -18,9 +32,27 @@ struct rp_register {
     std::string notification;
 };
 
+struct rq_login {
+    MessageType type = RQ_LOGIN;
+    std::string username;
+    std::string password;
+};
+
+struct rp_login {
+    MessageType type = RP_LOGIN;
+    bool accept;
+    std::string notification;
+};
+
 void struct_to_message(void *p, MessageType type, char *output);
 
 rq_register *message_to_rq_register(char *message);
 rp_register *message_to_rp_register(char *message);
 
-void split(char *input, const char *delimiter, std::vector<char *> *output);
+//TODO
+rq_login *message_to_rq_login(char *message);
+rp_login *message_to_rp_login(char *message);
+
+
+//Phân tách input bằng delimiter và trả về vector 
+std::vector<char *> split(char *input, const char *delimiter);
--- src/Message.cpp
+++ src/Message.cpp
@@ -2,18 +2,21 @@
 
 using namespace std;
 
-void split(char *input, const char *delimiter, vector<char *> *output) {
+vector<char *> split(char *input, const char *delimiter) {
+    vector<char *> res;
+    
     char *token;
-
     token = strtok(input, delimiter);
 
     while(token != NULL) {
         char* temp = (char *)malloc(strlen(token) + 1);
         strcpy(temp, token);
-        output->push_back(temp);
+        res.push_back(temp);
 
         token = strtok(NULL, delimiter);
     }
+
+    return res;
 }
 
 void struct_to_message(void *p, MessageType type, char *output) {
@@ -33,19 +36,21 @@ void struct_to_message(void *p, MessageType type, char *output) {
         strcpy(output, temp.c_str());
         break;
     }
+    case RP_REGISTER:
+        break;
+
     default:
         break;
     }
 }
 
 rq_register *message_to_rq_register(char *message) {
-    vector <char *> *splited_line = new vector<char *>();
-    split(message, "\n", splited_line);
+    vector <char *> splited_line = split(message, "\n");
 
     rq_register *res = new rq_register;
     res->type = RQ_REGISTER;
-    res->username = splited_line->at(1);
-    res->password = splited_line->at(2);
+    res->username = splited_line.at(1);
+    res->password = splited_line.at(2);
 
     return res;
 }
